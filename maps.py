378591b8dd035fd6ab28dfcc0ebfc940b36d6322
Knikolskiy <glucksistemi@gmail.con>
2017-02-14 10:30:07 +0300

    in process of creating more unified algorythm

--- maps.py
+++ maps.py
@@ -1,5 +1,5 @@
 import random
-
+import math
 #params
 
 
@@ -9,13 +9,106 @@ WALL = 'X'
 EMPTY = ' '
 
 
+class Map: # class for map and it's creation
+    directions = [  # posible direction of movement
+        {'swap': 1, 'add': -1},  # up (y-)
+        {'swap': 0, 'add': 1},  # right (x+)
+        {'swap': 1, 'add': 1},  # down (y+)
+        {'swap': 0, 'add': -1}  # left (x-)
+    ]
+
+    def __init__(self, width, height):
+        self.map = [[EMPTY for w in range(height)] for h in range(width)]  # THE CREATION OF MATRIX
+        self.width = width
+        self.height = height
+        self.xy = (width,height)
+
+    @staticmethod
+    def choose_direction(pos, direction, collision):
+        dirs = [0,1,2,3]
+        dirs.pop(dirs.index(direction))
+        for col in collision:
+            try:
+                dirs.pop(dirs.index(col))
+            except (IndexError, ValueError):
+                pass
+        return random.choice(dirs)
+
+    @staticmethod
+    def opposite(direction):
+        if direction == 0:
+            return 2
+        if direction == 1:
+            return 3
+        if direction == 2:
+            return 0
+        if direction == 3:
+            return 1
+
+    def draw_path(
+            self,
+            main_direction,
+            x_range,
+            y_range,
+            sign=PATH,
+            start_coords=None,
+            avoid_sign=(),
+            start_sign=None,
+            end_sign=None):
+
+        def wall_collision(): # checking walls collisions/ returns the direction of the collided wall
+            cols = []
+            if pos[1] <= y_range[0]:
+                cols.append(0)
+            if pos[0] >= x_range[1]:
+                cols.append(1)
+            if pos[1] >= y_range[1]:
+                cols.append(2)
+            if pos[0] <= x_range[0]:
+                cols.append(3)
+            return  cols
+
+        if not start_sign:
+            start_sign = sign
+        if not end_sign:
+            end_sign = sign
+        direction = main_direction
+
+        if start_coords:
+            pos = start_coords
+        else:
+            if direction == 0:
+                pos = [random.randint(*x_range), y_range[1]]
+            elif direction == 1:
+                pos = [x_range[0], random.randint(*y_range)]
+            elif direction == 2:
+                pos = [random.randint(*x_range), y_range[0]]
+            else:
+                pos = [x_range[1], random.randint(*y_range)]
+
+        while main_direction not in wall_collision():
+            coll = wall_collision()
+            if coll and direction in coll:
+                direction = self.choose_direction(pos, direction, coll)
+
+
+
+
+
+
+
 def rand_bool(chance):
     return random.randint(0,100) < chance
 
 
 def create_level_map(x, y):
     xy = (x, y)
-
+    directions = [  # posible direction of movement
+        {'swap': 1, 'add': -1},  # up (y-)
+        {'swap': 0, 'add': 1},  # right (x+)
+        {'swap': 1, 'add': 1},  # down (y+)
+        {'swap': 0, 'add': -1}  # left (x-)
+    ]
     def choose_direction(pos):
         if pos[0] == 0:
             if pos[1] == 0:
@@ -60,12 +153,7 @@ def create_level_map(x, y):
             return xy[directions[direction]['swap']]-1
     lmap = [[EMPTY for w in range(y)] for h in range(x)] # THE CREATION OF MATRIX
     # creating path
-    directions = [ # posible direction of movement
-        {'swap': 1, 'add':-1}, # up (y-)
-        {'swap': 0, 'add': 1}, # right (x+)
-        {'swap': 1, 'add': 1}, # down (y+)
-        {'swap': 0, 'add':-1}  # left (x-)
-    ]
+
     path_start = random.randint(0,y-1) # random start point on the left side of field
     path_pos = [0,path_start]
     direction = -1 # direction will be reseted anyway, but it should have initial value
